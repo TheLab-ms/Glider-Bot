4fe393c92c8b093bff6ef60a746a06515a19b2b5
Cameron Steele <cam.steeleis@gmail.com>
2021-02-24 13:32:18 -0600

    Basic Features

--- listeners/ready.js
+++ listeners/ready.js
@@ -8,8 +8,12 @@ class ReadyListener extends Listener {
         });
     }
 
-    exec() {
-        console.log('I\'m ready!');
+    exec(a) {
+        console.log("I'm ready!");
+        this.client.user.setActivity("A Game Called Life")
+        /*console.log(this.client.guilds.cache.get("690314626289631294").channels.cache.array().filter(channel => channel.type == "text").map(channel => {
+            return {id: channel.id, name: channel.name}
+        }));*/
     }
 }
 
