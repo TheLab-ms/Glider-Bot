701dc9d1b18982622117f2358b52d301b1d5e6a7
Cameron Steele <cam.steeleis@gmail.com>
2021-02-24 21:57:45 -0600

    added basic verification

--- listeners/ready.js
+++ listeners/ready.js
@@ -11,9 +11,6 @@ class ReadyListener extends Listener {
     exec(a) {
         console.log("I'm ready!");
         this.client.user.setActivity("A Game Called Life")
-        /*console.log(this.client.guilds.cache.get("690314626289631294").channels.cache.array().filter(channel => channel.type == "text").map(channel => {
-            return {id: channel.id, name: channel.name}
-        }));*/
     }
 }
 
