167b5187f17e2c1f91108fbeebd367204dc00ad8
Cameron Steele <cam.steeleis@gmail.com>
2021-02-25 09:27:29 -0600

    Added Debug command placeholder

---
+++ commands/debug.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord-akairo');
+
+class DebugCommand extends Command {
+    constructor() {
+        super('debug', {
+            aliases: ['debug'],
+            args: [
+                {
+                    id: "action",
+                    type: ["purge"]
+                }
+            ]
+        })
+    }
+
+    exec(message, args){
+        if(!message.member.roles.find(r => r.name === "Administrator") || !message.member.roles.find(r => rname === "Leadership")){
+            message.reply("That doesn't sound like anything I know");
+            //message.react("?")
+            return;
+        }
+        if(!args.hasOwnProperty("action")){
+            message.reply("no action was specified")
+            return;
+        }
+    }
+}
+
+module.exports = DebugCommand;
